53f471b241d8587fd69ca1b2cddc23425807ca88
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-05-07 13:23:04 +0100

    more visible highlighting for quited code

--- assets/js/highlight-tags.js
+++ assets/js/highlight-tags.js
@@ -2,17 +2,19 @@
 // This is verbose and it doesn't trim content of "code" tag so the first line 
 // has to be after the tag and this looks ugly. 
 function transformLanguageToCodeTagsOnLoad(hljs) {
-    document.addEventListener("DOMContentLoaded", function(event) {
+	document.addEventListener("DOMContentLoaded", function(event) {
 		var languages = ["groovy", "kotlin", "java", "lua", "javascript", "scheme"];
-        languages.forEach(function(language) {
-            $.each($(language), function(n, it) {
-                var element = $(it);
-                element = element.replaceWith(
-                    "<pre><code class='"+ language + "'>" + element.html().trim() + "</code></pre>"
-                );
-            });
-        });
-		hljs.configure({tabReplace: '    '});
+		languages.forEach(function(language) {
+			$.each($(language), function(n, it) {
+				var element = $(it);
+				element = element.replaceWith(
+					"<pre><code class='" + language + "'>" + element.html().trim() + "</code></pre>"
+				);
+			});
+		});
+		hljs.configure({
+			tabReplace: '    '
+		});
 		hljs.initHighlighting();
-    });
+	});
 }
