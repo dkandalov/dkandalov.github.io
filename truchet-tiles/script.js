99a0d662bdfae24ca32a60e8cc5c0fb375dd817b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2023-06-29 08:45:22 +0100

    more patterns

--- truchet-tiles/script.js
+++ truchet-tiles/script.js
@@ -1,6 +1,7 @@
+const sourceContext = document.getElementById("source-canvas").getContext("2d");
+const maskContext = document.getElementById("mask-canvas").getContext("2d");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-const sourceContext = document.getElementById("source-canvas").getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 let tileSize = 30;
@@ -14,9 +15,9 @@ function sequential(drawFunctions) {
 }
 
 function square(size, drawFunctions) {
-    return (x, y, t) => {
-        let tileY = (y / tileSize) % size;
-        let tileX = (x / tileSize) % size;
+    	return (x, y, t) => {
+		let tileX = (x / tileSize) % size;
+		let tileY = (y / tileSize) % size;
         drawFunctions[tileY * size + tileX](x, y, t)
     };
 }
@@ -61,20 +62,61 @@ const d = (x, y, t) => {
     context.fill();
 };
 
+const diagonalsWithMaskImage = (x, y, t) => {
+	context.beginPath();
+	context.lineWidth = t * 10;
+	context.lineCap = "round";
+	const data = maskContext.getImageData(x, y, tileSize, tileSize).data;
+	if (rgbMean(data) <= 150) {
+		context.moveTo(x, y);
+		context.lineTo(x + tileSize, y + tileSize);
+	} else {
+		context.moveTo(x, y + tileSize);
+		context.lineTo(x + tileSize, y);
+	}
+	context.shadowColor = "black";
+	context.shadowBlur = 5;
+    context.stroke();
+};
+
+const diamond = (x, y, t) => {
+	let scale = 4;
+	let column = Math.floor(x / (tileSize * scale));
+	let row = Math.floor(y / (tileSize * scale));
+
+	context.beginPath();
+	context.lineWidth = t * 10;
+	context.lineCap = "round";
+	if ((row % 2 !== 0 && column % 2 === 0) || (row % 2 === 0 && column % 2 !== 0)) {
+		context.moveTo(x, y);
+		context.lineTo(x + tileSize, y + tileSize);
+	} else {
+		context.moveTo(x, y + tileSize);
+		context.lineTo(x + tileSize, y);
+	}
+	context.shadowColor = "black";
+	context.shadowBlur = 5;
+    context.stroke();
+};
+
+const rgbMean = (imageData) => {
+	let sum = 0;
+	for (let i = 0; i < imageData.length; i += 4) {
+		let r = imageData[i];
+		let g = imageData[i + 1];
+		let b = imageData[i + 2];
+		sum += r + g + b;
+	}
+	return sum / ((imageData.length / 4) * 3);
+}
+
 const drawTiledImage = (drawTile) => {
     context.clearRect(0, 0, width, height);
 
     for (let y = 0; y < height; y += tileSize) {
         for (let x = 0; x < width; x += tileSize) {
             const tileData = sourceContext.getImageData(x, y, tileSize, tileSize).data;
-            let sum = 0;
-            for (let i = 0; i < tileData.length; i += 4) {
-                let r = tileData[i];
-                let g = tileData[i + 1];
-                let b = tileData[i + 2];
-                sum += r + g + b;
-            }
-            const mean = sum / ((tileData.length / 4) * 3);
+            const mean = rgbMean(tileData);
             const t = Math.max(0.25, Math.min(0.75, 1 - mean / 255));
 
             drawTile(x, y, t);
@@ -94,6 +136,8 @@ document.addEventListener('keydown', (e) => {
 
 let patternIndex = 0;
 const patterns = [
+	diagonalsWithMaskImage,
+	diamond,
 	square(4, [ // waves
 		a, c, b, d,
 		c, a, d, b,
@@ -135,9 +179,17 @@ const patterns = [
 const main = () => {
     drawTiledImage(patterns[patternIndex]);
 };
+
 const image = new Image();
 image.addEventListener('load', function() {
 	sourceContext.drawImage(image, 0, 0, width, height);
 	main();
 });
 image.src = '/assets/images/me.jpg';
+
+const image2 = new Image();
+image2.addEventListener('load', function() {
+	maskContext.drawImage(image2, 0, 0, width, height);
+	main();
+});
+image2.src = '/assets/images/code.jpg';
